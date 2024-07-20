3c31a1835164be35c5c3d362d0e9d5c1bb33f03a
EC2 Default User <ec2-user@ip-172-31-39-84.eu-west-1.compute.internal>
2024-07-20 23:48:40 +0000

    Updates

--- Inventory-Management-System-Django-AWS/inventory_main/settings.py
+++ Inventory-Management-System-Django-AWS/inventory_main/settings.py
@@ -170,6 +170,6 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL =  None
 AWS_S3_VERITY = True
 #STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage' #Boto3 client used to use S3 storage instead of Django local storage
+#DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage' #Boto3 client used to use S3 storage instead of Django local storage
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
